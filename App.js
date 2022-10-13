a325b5a1129b4f3341df9bffbd63f45ee408c3ea
nguyenhuunghi9999 <80930428+nguyenhuunghi9999@users.noreply.github.com>
2022-10-13 17:19:27 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -1,66 +1,168 @@
-import React, { useState } from "react";
-import {StyleSheet, Text, View, TouchableOpacity, ScrollView, Alert, } from "react-native";
-import Task from "./components/Task";
-import Form from "./components/Form";
+import * as React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  Image,
+} from "react-native";
 
-const App = () => {
-  const [taskList, setTaskList] = useState([]);
-  const handleAddTask = (task) => {
-    setTaskList([...taskList, task]);
+export default class App extends React.Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      newItem: "",
+      listOfItems: [],
+    };
+  }
+  
+
+  deleteItem(id) {
+    //get all the list items
+    const list = this.state.listOfItems;
+    //filter the list and create another list which is basically list - item to be deleted
+    const updatedList = list.filter((item) => item.id !== id);
+    //now set the state with updated list
+    this.setState({
+      listOfItems: updatedList,
+    });
+  }
+  updateInput(key, value) {
+    // update react state
+    this.setState({
+      [key]: value,
+    });
+  }
+  addItem = () => {
+    if (this.state.newItem != "") {
+      // create a new item with unique id
+      const newItemJSON = {
+        id: 1 + Math.random(),
+        value: this.state.newItem.slice(),
+      };
+
+      // copy current list of items
+      const list = this.state.listOfItems;
+
+      // add the new item to the list
+      list.push(newItemJSON);
+
+      // update state with new list, reset the new item input
+      this.setState({
+        listOfItems: list,
+        //listOfItems:[...this.state.listOfItems,list],
+        newItem: "",
+      });
+    }
   };
-  const handleDeleteTask = (index) => {
-    Alert.alert("Thông báo !", "Bạn có chắc chắn muốn xóa?", [
-      {
-        text: "OK",
-        onPress: () => {
-          let taskListTmp = [...taskList];
-          taskListTmp.splice(index,1);
-          setTaskList(taskListTmp);
-        },
-      },
-      { text: "Cancel", onPress: () => {} },
-    ]);
-  };
-  return (
-    <View style={styles.container}>
-      <View style={styles.body}>
-        <Text style={styles.header}>List</Text>
-        <ScrollView style={styles.items}>
-          {taskList.map((item, index) => {
-            return (
-              <Task
-                key={index}
-                title={item}
-                number={index + 1}
-                onDeleteTask={() => handleDeleteTask(index)}
-              />
-            );
-          })}
-        </ScrollView>
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.textView}>
+          <Text style={styles.text}>To Do List</Text>
+        </View>
+        <View>
+          <TextInput
+            placeholder="  Type here ..."
+            style={styles.inputBox}
+            onChangeText={(text) => {
+              this.setState({ newItem: text });
+            }}
+            value={this.state.newItem}
+          ></TextInput>
+          <View>
+            <TouchableOpacity style={styles.button} onPress={this.addItem}>
+              <Text style={styles.buttontext}>+</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View>
+            <ScrollView>
+              {this.state.listOfItems.map((item) => {
+                return (
+                  <View style={styles.listview}>
+                     <Text style={styles.textstyle}> {item.i}</Text>
+                    <Text style={styles.textstyle}> {item.value}</Text>
+                    <TouchableOpacity
+                      style={{
+                        backgroundColor: "#16cc62",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        padding: 10,
+                      }}
+                      onPress={() => this.deleteItem(item.id)}
+                    >
+                      <Text style={{ color: "white" }}>Delete</Text>
+                    </TouchableOpacity>
+                  </View>
+                );
+              })}
+            </ScrollView>
+          </View>
+        </View>
       </View>
-      <Form onAddTask={handleAddTask} />
-    </View>
-  );
-};
-
-export default App;
-
+    );
+  }
+}
 const styles = StyleSheet.create({
   container: {
-      flex: 1,
-      backgroundColor: "#eff7f8",
-    },
-    body: {
-      flex: 1,
-      paddingTop: 50,
-      paddingHorizontal: 20,
-    },
-    header: {
-      fontSize: 24,
-      color: "#21a3d0",
-      fontWeight: "bold",
-    },
-    items: {
-      marginTop: 25,
-    }
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: 30,
+  },
+  textView: {
+    backgroundColor: "white",
+    height: 80,
+  },
+  text: {
+    textAlign: "center",
+    marginTop: "10%",
+    fontSize: 25,
+    color: "#16cc62",
+    fontWeight: "bold",
+  },
+  inputBox: {
+    borderWidth:1,
+    borderRadius:10,
+    backgroundColor: "white",
+    textAlign: "center",
+    fontSize: 20,
+    height: 50,
+    margin:20
+  },
+  button: {
+    position: "absolute",
+    right: 20,
+    top: 200,
+    backgroundColor: "#16cc62",
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    elevation: 8,
+  },
+
+  buttontext: {
+    color: "#fff",
+    fontSize: 24,
+  },
+  textstyle: {
+    fontSize: 20,
+    color: "#16cc62",
+  },
+  listview: {
+    borderWidth: 2,
+    height: 40,
+    justifyContent: "space-between",
+    borderColor: "#16cc62",
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
 });
